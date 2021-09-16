240794642068f57b760b0deb5cb2b18a3ee8c64f
Carlos Barbachano <cbarb035@fiu.edu>
2021-09-16 19:47:09 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App bg-light">
+      <button type="button" className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
